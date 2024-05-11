efe1fc7e9252f48cecd9c50182021c7d638e227e
Seema-476 <simmybishnoi29085@gmail.com>
2024-05-11 12:29:34 +0530

    nav done

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function opennav() {
+    document.getElementById("navlist").classList.toggle("start-0");
+    document.querySelector(".menuicon").classList.toggle("cross");
+    document.body.classList("overflowhidden");
+}
