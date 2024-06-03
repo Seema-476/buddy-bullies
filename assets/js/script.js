d3d3719a55e4b263427089cf86c5c4dbdca54f0b
Seema-476 <simmybishnoi29085@gmail.com>
2024-06-03 14:01:48 +0530

    add down to top btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ function opennav() {
     document.querySelector(".menuicon").classList.toggle("cross");
     document.body.classList.toggle("overflowhidden");
 }
+// scroll top btn 
+const topBtn = document.querySelector(".topBtn")
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show")
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
